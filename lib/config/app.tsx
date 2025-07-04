63e4c6eaf3c19342b21eb0c66b087b5e8ed4b74e
Michael Hurhangee <m.hurhangee@me.com>
2025-07-04 09:42:01 +0000

    fonts, config and cleanup

---
+++ lib/config/app.tsx
@@ -0,0 +1,5 @@
+export const appConfig = {
+  appName: 'My Corner of the Web | Michael Hurhangee',
+  appDescription: 'A portfolio, blog, resume and playground for Michael Hurhangee',
+  emojiFavicon: '🔲',
+}
