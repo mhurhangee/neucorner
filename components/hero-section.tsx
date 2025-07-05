20848e7a9517b0f09fd385ed7f4e9397af46f788
Michael Hurhangee <m.hurhangee@me.com>
2025-07-05 18:33:42 +0000

    mobile fixes to external link icon and heading size

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -5,9 +5,9 @@ import { Section } from './section'
 export const HeroSection = () => {
   return (
     <Section id="home">
-      <h1>
-        my <br />
-        webcorner <br />
+      <h1 className="pt-0">
+        my <br />web <br />corner <br />
+
       </h1>
       <p className="pb-8">
         <Link href="#about">about</Link>, <Link href="#posts">posts</Link> and{' '}
